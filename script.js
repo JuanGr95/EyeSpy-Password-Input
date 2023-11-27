bc510ca3181518716f6bffe56ba361868f2fa61d
JuanGr95 <juangrossosicre@gmail.com>
2023-11-27 19:35:20 +0100

    Commit upgrade funciones de comportamiento

--- script.js
+++ script.js
@@ -4,85 +4,80 @@ const circuloInterno = document.getElementById('circuloInterno');
 const circuloExterno = document.getElementById('circuloExterno');
 const eyeUp = document.getElementById('up');
 const eyeGroup = document.getElementById('eyeGroup');
-var randomInterval = Math.floor(Math.random() * 15000);
 let stopBlink = false;
 
-function blink() {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-    function animateBlink() {
+async function blink() {
+    while (!stopBlink) {
         const randomInterval = Math.floor(Math.random() * (10000 - 3000 + 1)) + 3000;
-
-        setTimeout(() => {
-            if (!stopBlink) {
-                eyeUp.style.transform = "scaleY(-1)";
-                eyeGroup.setAttribute('mask', 'url(#eye-closed)');
-
-                setTimeout(() => {
-                    if (!stopBlink) {
-                        eyeUp.style.transform = "scaleY(1)";
-                        eyeGroup.setAttribute('mask', 'url(#eye-open)');
-                        animateBlink();
-                    }
-                }, 160);
-            }
-        }, randomInterval);
+        await sleep(randomInterval);
+        eyeUp.style.transform = "scaleY(-1)";
+        eyeGroup.setAttribute('mask', 'url(#eye-closed)');
+        await sleep(160);
+        eyeUp.style.transform = "scaleY(1)";
+        if (!stopBlink)
+            eyeGroup.setAttribute('mask', 'url(#eye-open)');
     }
-    animateBlink();
 }
 
 blink();
 
-function togglePasswordVisibility() {
-    const passwordInput = document.getElementById('password');
-    const passwordValue = passwordInput.value;
-    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~,.<>?/;":][}{+_)(*&^%$#@!±=-§';
 
-    if (passwordInput.type == 'password' && passwordInput.value) {
-        let index = 0;
+const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~,.<>?/;":][}{+_)(*&^%$#@!±=-§';
+const charStepTime = 15;
+const charStepSize = 10;
 
-        stopBlink = true;
-        eyeGroup.setAttribute('mask', 'url(#eye-closed)');
-        eyeUp.style.display = 'none';
+async function revealPassword(passwordInput, passwordValue) {
+    let index = 1;
 
-        const intervalId = setInterval(() => {
-            const revealedValue = passwordValue.substring(0, index) + '•'.repeat(passwordValue.length - index);
+    stopBlink = true;
+    eyeGroup.setAttribute('mask', 'url(#eye-closed)');
+    eyeUp.style.display = 'none';
+    passwordInput.type = 'text';
 
-            function generateChar() {
-                if (index < passwordValue.length) {
-                    let charTemp = chars.charAt(Math.floor(Math.random() * chars.length));
-                    let valueTemp = index > 0 ? revealedValue.substring(0, index - 1) : '';
-                    passwordInput.value = valueTemp + charTemp + '•'.repeat(passwordValue.length - index);
-                }
-            }
+    while (index <= passwordValue.length) {
+        const revealedValue = passwordValue.substring(0, index) + '•'.repeat(passwordValue.length - index);
 
-            generateChar();
+        for (let i = 0; i < charStepSize; i++) {
+            await generateChar(index, revealedValue, passwordInput, passwordValue);
+        }
 
-            const intervalCl = setInterval(generateChar, 10);
-
-            setTimeout(() => {
-                clearInterval(intervalCl);
-            }, 100);
-
-            index++;
-
-            passwordInput.value = revealedValue;
-
-            if (index > passwordValue.length) {
-                clearInterval(intervalId);
-            } else {
-                passwordInput.type = 'text';
-            }
-        }, 120);
-
-    } else if (passwordInput.type == 'text' && passwordInput.value) {
-        eyeGroup.setAttribute('mask', 'url(#eye-open)');
-        eyeUp.style.display = 'block';
-        stopBlink = false;
-        blink();
-        passwordInput.type = 'password';
+        passwordInput.value = revealedValue;
+        index++;
     }
 }
 
+async function generateChar(index, revealedValue, passwordInput, passwordValue) {
+    if (index <= passwordValue.length) {
+        let charTemp = chars.charAt(Math.floor(Math.random() * chars.length));
+        let valueTemp = index > 0 ? revealedValue.substring(0, index - 1) : '';
+        passwordInput.value = valueTemp + charTemp + '•'.repeat(passwordValue.length - index);
+    }
+
+    await sleep(charStepTime);
+}
+
+function hidePassword(passwordInput) {
+    eyeGroup.setAttribute('mask', 'url(#eye-open)');
+    eyeUp.style.display = 'block';
+    stopBlink = false;
+    blink();
+    passwordInput.type = 'password';
+}
+
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const passwordValue = passwordInput.value;
+
+    if (passwordInput.type == 'password' && passwordValue) {
+        revealPassword(passwordInput, passwordValue);
+    } else if (passwordInput.type == 'text' && passwordValue) {
+        hidePassword(passwordInput);
+    }
+}
 
 window.addEventListener('mousemove', (event) => {
     const rect = svgContainer.getBoundingClientRect();
